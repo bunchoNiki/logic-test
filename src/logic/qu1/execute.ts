974af7f3eec901ef0c99c2b432a9dc39e7002591
bunchoNiki <yuta.yamazaki@allsmart.co.jp>
2025-07-21 15:10:11 +0900

    [add]: 回答

--- src/logic/qu1/execute.ts
+++ src/logic/qu1/execute.ts
@@ -1,4 +1,11 @@
+import { FOUR_OF_A_KIND, FULL_HOUSE, HIGH_CARD, ONE_PAIR, STRAIGHT, THREE_OF_A_KIND, TWO_PAIR } from "./constants";
 
+const HAND_TYPE: Record<number, string> = {
+  [5]: HIGH_CARD,
+  [4]: ONE_PAIR,
+  [3]: TWO_PAIR,
+  [2]: FULL_HOUSE,
+};
 /**
   ストレート → 数字が連続している
   4カード   → 5枚中4枚同一の数字
@@ -20,6 +27,23 @@
   【条件】
 **/
 export const main = (hand: string): string => {
-  return hand;
+  if (/(\d)\1{3}/.test(hand)) {
+    return FOUR_OF_A_KIND;
+  }
+
+  const arr = hand.split('').map(Number).sort();
+  const set = new Set(arr);
+
+  if (set.size === 3 && /(\d)\1{2}/.test(hand)) {
+    return THREE_OF_A_KIND;
+  }
+
+  if (set.size === 5 && arr[0] * 5 + 10 === arr.reduce((n, h) => n + h, 0)) {
+    return STRAIGHT;
+  }
+
+  return HAND_TYPE[set.size];
 };
+
+
 main('13535');
