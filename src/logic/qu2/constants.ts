ebab68c2d1609b051fff9d4757f745124e426818
bunchoNiki <yuta.yamazaki@allsmart.co.jp>
2025-08-15 20:13:56 +0900

    [add]: ボーリングの各スコアの計算

---
+++ src/logic/qu2/constants.ts
@@ -0,0 +1,17 @@
+export const STRIKE_BASE_SCORE = 10;
+export const SPARE_BASE_SCORE = 10;
+
+export type PinCount = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+type Tuple<T, N extends number, R extends T[] = []> =
+  R['length'] extends N
+  ? R
+  : Tuple<T, N, [...R, T]>;
+
+export type RegularFrame = [PinCount, PinCount];
+
+export type FinalFrame = [PinCount, PinCount, PinCount];
+
+export type FirstNineFrames = Tuple<RegularFrame, 9>;
+
+export type BowlingFrames = [...FirstNineFrames, FinalFrame];
---
+++ src/logic/qu2/execute.ts
@@ -0,0 +1,10 @@
+import { BowlingFrames } from "./constants";
+
+
+/**
+ * @param {BowlingFrames} frames 1ゲームの全フレームの配列
+ * @returns {Array<number>} 各フレームのスコア
+ */
+export const main = (frames: BowlingFrames): Array<number> => {
+  return frames.map(f => f[0]);
+};
