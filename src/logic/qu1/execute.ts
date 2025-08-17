589e118163286556e8c709dbb31bf8ba857d453d
shintaro.mogi <shintaro.mogi@allsmart.co.jp>
2025-08-18 07:39:59 +0900

    [fix]メソッド分割

--- src/logic/qu1/execute.ts
+++ src/logic/qu1/execute.ts
@@ -8,6 +8,84 @@ import {
   TWO_PAIR,
 } from "./constants";
 
+const countDuplicatingNumber = (
+  numberArray: number[]
+): Record<number, number> => {
+  const counts: Record<number, number> = {};
+  for (const num of numberArray) {
+    counts[num] = (counts[num] || 0) + 1;
+  }
+  return counts;
+};
+
+type DuplicatingKinds = {
+  hasFour: number;
+  hasThree: number;
+  pairCount: number;
+};
+
+const checkDuplicatingKinds = (
+  counts: Record<number, number>
+): DuplicatingKinds => {
+  const duplicating: DuplicatingKinds = {
+    hasFour: 0,
+    hasThree: 0,
+    pairCount: 0,
+  };
+
+  for (const key in counts) {
+    const count: number = counts[key];
+    if (count === 4) {
+      // 4カードの場合は他の役の可能性はないため、早期リターンでよい
+      duplicating.hasFour++;
+      return duplicating;
+    }
+
+    if (count === 3) {
+      duplicating.hasThree++;
+    }
+
+    // 2以上だと後続の判定でペア役に吸われる可能性がある
+    if (count === 2) {
+      duplicating.pairCount++;
+    }
+  }
+
+  return duplicating;
+};
+
+const judgeRole = (
+  duplicatingKinds: DuplicatingKinds,
+  numberArray: number[]
+): string => {
+  const { hasFour, hasThree, pairCount } = duplicatingKinds;
+
+  // 役に必要なcountが多い役から判定
+  if (hasFour) {
+    return FOUR_OF_A_KIND;
+  }
+  if (hasThree) {
+    // フルハウスの判定が先
+    return pairCount === 1 ? FULL_HOUSE : THREE_OF_A_KIND;
+  }
+  if (hasFour) {
+    return TWO_PAIR;
+  }
+  if (pairCount === 1) {
+    return ONE_PAIR;
+  }
+
+  const sorted: number[] = [...numberArray].sort((a, b) => a - b);
+  //ペアがないかを確認しているためこの判定でよい
+  const isConsecutive: boolean = sorted[4] - sorted[0] === 4;
+
+  if (isConsecutive) {
+    return STRAIGHT;
+  }
+
+  return HIGH_CARD;
+};
+
 /**
  *
  * @param {string} hand 役を判定すべき手札
@@ -18,54 +96,14 @@ export const main = (hand: string): string => {
   if (!/^[1-9]{5}$/.test(hand)) {
     return HIGH_CARD;
   }
-  const numberArray = hand.split("").map((char) => parseInt(char));
 
-  const counts: Record<number, number> = {};
-  for (const num of numberArray) {
-    counts[num] = (counts[num] || 0) + 1;
-  }
+  const numberArray: number[] = hand.split("").map((char) => parseInt(char));
 
-  let hasThree = false;
-  let pairCount = 0;
+  const counts: Record<number, number> = countDuplicatingNumber(numberArray);
+  const duplicating: DuplicatingKinds = checkDuplicatingKinds(counts);
+  const role: string = judgeRole(duplicating, numberArray);
 
-  for (const key in counts) {
-    const count = counts[key];
-    if (count === 4) {
-      // 4カードの場合は他の役の可能性はないため、早期リターンでよい
-      return FOUR_OF_A_KIND;
-    }
-
-    if (count === 3) {
-      hasThree = true;
-    }
-
-    // 2以上だと後続の判定でペア役に吸われる可能性がある
-    if (count === 2) {
-      pairCount++;
-    }
-  }
-
-  // 役に必要なcountが多い役から判定
-  if (hasThree) {
-    // フルハウスの判定が先
-    return pairCount === 1 ? FULL_HOUSE : THREE_OF_A_KIND;
-  }
-  if (pairCount === 2) {
-    return TWO_PAIR;
-  }
-  if (pairCount === 1) {
-    return ONE_PAIR;
-  }
-
-  const sorted = [...numberArray].sort((a, b) => a - b);
-  //ペアがないかを確認しているためこの判定でよい
-  const isConsecutive = sorted[4] - sorted[0] === 4;
-
-  if (isConsecutive) {
-    return STRAIGHT;
-  }
-
-  return HIGH_CARD;
+  return role;
 };
 
 main("13535");
