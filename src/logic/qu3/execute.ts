228c04f61f53584a87c374f931a4fe1275d0a9c3
bunchoNiki <y.yamazaki@accenture.com>
2025-08-27 18:44:24 +0900

    [add]: エンコードの処理を追加

--- src/logic/qu3/execute.ts
+++ src/logic/qu3/execute.ts
@@ -5,7 +5,23 @@ const ALPHABET = LOWERCASE_ALPHABET + UPPERCASE_ALPHABET;
 type method =  (word: string, key: string) => string;
 
 const decoding: method = (word, key) => {
-  return word + key;
+  const keyMap = convertKeyMap(key);
+  let skipCount = 0;
+  return [...word].map((char, index) => {
+    if (!/^[a-zA-Z]+$/.test(char)) {
+      skipCount++;
+      return char;
+    }
+    
+    const wordIndex = ALPHABET.indexOf(char);
+    const keyIndex = keyMap[(index - skipCount) % key.length];
+
+    // 大文字小文字の処理をスキップするために大文字の場合は+26して大文字のある番地に変換
+    const added = ALPHABET_SIZE * Math.floor(wordIndex / ALPHABET_SIZE);
+    const charIndex = (wordIndex - keyIndex + ALPHABET_SIZE) % ALPHABET_SIZE + added;
+    return ALPHABET.slice(charIndex, charIndex + 1);
+
+  }).join('');
 };
 
 const encoding: method = (word, key)  => {
