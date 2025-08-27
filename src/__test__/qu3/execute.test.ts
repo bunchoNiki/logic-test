fcd3421dfea7fe690f72e4157fbd88655bdee27d
bunchoNiki <y.yamazaki@accenture.com>
2025-08-27 15:13:44 +0900

    [update]: テストファイルの配置位置の変更と特定の文字が失敗しないかを検するケースを追加

--- src/__test__/qu3/execute.test.ts
+++ src/__test__/qu3/execute.test.ts
@@ -1,24 +1,20 @@
-import { Mode, testCases } from '@/logic/qu3/constants';
+import { decrypt, enCrypt, testCases } from '@/logic/qu3/constants';
 import { main } from '@/logic/qu3/execute';
 
 describe('ヴィジュネル暗号 - 暗号化', () => {
-
-
   test.each(testCases)(
     '$name の場合、正しく暗号化されること',
-    ({ text, key, mode, expected }) => {
-      expect(main(text, key, mode as Mode)).toBe(expected);
+    ({ text, key, expected }) => {
+      expect(main(text, key, enCrypt)).toBe(expected);
     }
   );
 });
 
 describe.skip('ヴィジュネル暗号 - 復号', () => {
-
-
   test.each(testCases)(
     '$name の場合、正しく復号されること',
-    ({ text, key: expected, mode, expected: key }) => {
-      expect(main(text, key, mode as Mode)).toBe(expected);
+    ({ text, key: expected, expected: key }) => {
+      expect(main(text, key, decrypt)).toBe(expected);
     }
   );
 });
