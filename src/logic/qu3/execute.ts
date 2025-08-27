1c72f5e50bfd0f3272d0363b9e9a15309a5b809b
bunchoNiki <y.yamazaki@accenture.com>
2025-08-27 19:32:18 +0900

    [update]: 共通化してたらえらいことになった

--- src/logic/qu3/execute.ts
+++ src/logic/qu3/execute.ts
@@ -2,47 +2,6 @@ import { ALPHABET_SIZE, decrypt, enCrypt, LOWERCASE_ALPHABET, Mode, UPPERCASE_AL
 
 
 const ALPHABET = LOWERCASE_ALPHABET + UPPERCASE_ALPHABET;
-type method =  (word: string, key: string) => string;
-
-const decoding: method = (word, key) => {
-  const keyMap = convertKeyMap(key);
-  let skipCount = 0;
-  return [...word].map((char, index) => {
-    if (!/^[a-zA-Z]+$/.test(char)) {
-      skipCount++;
-      return char;
-    }
-    
-    const wordIndex = ALPHABET.indexOf(char);
-    const keyIndex = keyMap[(index - skipCount) % key.length];
-
-    // 大文字小文字の処理をスキップするために大文字の場合は+26して大文字のある番地に変換
-    const added = ALPHABET_SIZE * Math.floor(wordIndex / ALPHABET_SIZE);
-    const charIndex = (wordIndex - keyIndex + ALPHABET_SIZE) % ALPHABET_SIZE + added;
-    return ALPHABET.slice(charIndex, charIndex + 1);
-
-  }).join('');
-};
-
-const encoding: method = (word, key)  => {
-  const keyMap = convertKeyMap(key);
-  let skipCount = 0;
-  return [...word].map((char, index) => {
-    if (!/^[a-zA-Z]+$/.test(char)) {
-      skipCount++;
-      return char;
-    }
-
-    const wordIndex = ALPHABET.indexOf(char);
-    const keyIndex = keyMap[(index - skipCount) % key.length];
-
-    // 大文字小文字の処理をスキップするために大文字の場合は+26して大文字のある番地に変換
-    const added = ALPHABET_SIZE * Math.floor(wordIndex / ALPHABET_SIZE);
-    const charIndex = (wordIndex + keyIndex) % ALPHABET_SIZE + added;
-
-    return ALPHABET.slice(charIndex, charIndex  + 1);
-  }).join('');
-};
 
 /**
  * keyを参照しやすいMapへの変換処理を行う.
@@ -53,10 +12,42 @@ const convertKeyMap = (key: string): Record<number, number> => {
   return [...key].reduce((m, s, i) => ({ ...m, [i]: LOWERCASE_ALPHABET.indexOf(s) }), {});
 };
 
-const logic: Record<Mode, method> = {
-  [decrypt]: decoding,
-  [enCrypt]: encoding
+const getIndex = (textIndex: number, keyIndex: number): number => {
+  // 大文字小文字の処理をスキップするために大文字の場合は+26して大文字のある番地に変換
+  const added = ALPHABET_SIZE * Math.floor(textIndex / ALPHABET_SIZE);
+  
+  const shiftedIndex = textIndex % ALPHABET_SIZE + keyIndex;
+  const newNormalizedIndex = (shiftedIndex % ALPHABET_SIZE + ALPHABET_SIZE) % ALPHABET_SIZE;
+
+  return newNormalizedIndex + added;
 };
 
-export const main = (word: string, key: string, mode: Mode) => logic[mode](word, key);
+/**
+ * 中間処理を
+ */
+const logic: Record<Mode, typeof getIndex> = {
+  [decrypt]: (textIndex, keyIndex) => getIndex(textIndex, (keyIndex + ALPHABET_SIZE) * -1),
+  [enCrypt]: getIndex
+};
+
+export const main = (word: string, key: string, mode: Mode) => {
+  const keyMap = convertKeyMap(key);
+  let skipCount = 0;
+
+  const strArr = [...word].map((char, index) => {
+    if (!/^[a-zA-Z]+$/.test(char)) {
+      skipCount++;
+      return char;
+    }
+
+    const wordIndex = ALPHABET.indexOf(char);
+    const keyIndex = keyMap[(index - skipCount) % Object.keys(keyMap).length];
+    const charIndex = logic[mode](wordIndex, keyIndex);
+
+    return ALPHABET.slice(charIndex, charIndex  + 1);
+  });
+
+  return strArr.join('');
+};
+
 main('Hello World!', 'key', 'encrypt');
