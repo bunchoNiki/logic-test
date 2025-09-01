f1ef60e6d8ae55718d3d1b584c7b61eb21b57bba
bunchoNiki <yuta.yamazaki@allsmart.co.jp>
2025-09-01 10:09:49 +0900

    [add]: バグありサンプルコード

--- src/logic/qu3/execute.ts
+++ src/logic/qu3/execute.ts
@@ -1,7 +1,39 @@
-import { Mode } from './constants';
+import { ALPHABET_SIZE, LOWERCASE_ALPHABET, Mode, UPPERCASE_ALPHABET } from './constants';
 
-export const main = (word: string, key: string, mode: Mode) => {
-  return 'Rijvs Uyvjn!'.replace('',word).replace('', key).replace('', mode);
+export const main = (text: string, key: string, mode: Mode): string => {
+  let result = '';
+
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    const keyChar = key[i % key.length];
+
+    const upperIndex = UPPERCASE_ALPHABET.indexOf(char);
+    const lowerIndex = LOWERCASE_ALPHABET.indexOf(char);
+
+    if (upperIndex === -1 && lowerIndex === -1) {
+      result += char;
+      continue;
+    }
+
+    const isUppercase = upperIndex !== -1;
+    const textCharIndex = isUppercase ? upperIndex : lowerIndex;
+    const keyCharIndex = LOWERCASE_ALPHABET.indexOf(keyChar.toLowerCase());
+
+    let newIndex;
+    if (mode === 'encrypt') {
+      newIndex = (textCharIndex + keyCharIndex) % ALPHABET_SIZE;
+    } else {
+      newIndex = (textCharIndex - keyCharIndex + ALPHABET_SIZE) % ALPHABET_SIZE;
+    }
+
+    if (isUppercase) {
+      result += UPPERCASE_ALPHABET[newIndex];
+    } else {
+      result += LOWERCASE_ALPHABET[newIndex];
+    }
+  }
+
+  return result;
 };
 
 main('Hello World!', 'key', 'encrypt');
